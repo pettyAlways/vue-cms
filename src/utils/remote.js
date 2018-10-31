0c9ee389eb08202e3c3660a6c7f45a8c21c90cda
鹰嘴豆 <1036771966@qq.com>
2018-10-31 21:56:33 +0800

    [配置优化] 授权不通过，当前资源需要登录访问做判断

--- src/utils/remote.js
+++ src/utils/remote.js
@@ -19,20 +19,30 @@ axios.interceptors.request.use(function (config) {
 })
 
 axios.interceptors.response.use(function (response) {
+  // 重定向登录页面
+  if (response.data === 401) {
+    removeToken()
+    setTimeout(() => {
+      location.href = '/#/login'
+    }, 3 * 1000 + 1)
+    Vue.prototype.$message({
+      message: '请先登录再访问资源',
+      type: 'error'
+    })
+    return { flag: false }
+  }
+  // 重定向主页
+  if (response.data === 403) {
+    setTimeout(() => {
+      location.href = '/#/home'
+    }, 3 * 1000 + 1)
+    Vue.prototype.$message({
+      message: '没有权限访问',
+      type: 'error'
+    })
+    return { flag: false }
+  }
   if (!response.data.flag) {
-    // 未登陆
-    if (response.data.code === '401') {
-      removeToken()
-      setTimeout(() => {
-        location.href = '/#/login'
-      }, 3 * 1000 + 1)
-    }
-    // 未授权
-    if (response.data.code === '403') {
-      setTimeout(() => {
-        location.href = '/#/home'
-      }, 3 * 1000 + 1)
-    }
     // 预留一些错误不提示，如code为2018
     if (response.data.code !== '2018') {
       Vue.prototype.$message({
