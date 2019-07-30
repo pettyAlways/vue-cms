dd3bc395ead0b63055cc84ce3dcd5fd832682eb5
鹰嘴豆 <1036771966@qq.com>
2019-07-30 14:02:25 +0800

    [代码优化] 代码优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,30 +1,26 @@
 <template>
   <el-container class="login-container">
+    <div class="site-title">
+      <icon-svg iconClass="laoying" :vStyle="{width: '5em', height: '5em'}"></icon-svg>
+      <p class="title">欢迎来到知识库协作式网站</p>
+    </div>
     <el-card>
       <div slot="header">
-        <h2 class="login-title">{{$t('login.title')}}</h2>
+        <h2 class="login-title">登录</h2>
       </div>
       <el-form :rules="rules" :model="loginForm" ref="loginForm" label-width="60px">
-        <el-form-item :label="$t('login.account')" prop="userAccount" style="position:relative">
+        <el-form-item label="账号" prop="userAccount" style="position:relative">
           <el-input type="text" v-model="loginForm.userAccount" @keyup.enter.native="goToPwdInput"></el-input>
-          <span class="svg-container svg-container_user">
-            <icon-svg icon-class="user" />
-          </span>
         </el-form-item>
-        <el-form-item :label="$t('login.password')" prop="userPassword">
+        <el-form-item label="密码" prop="userPassword">
           <el-input type="password" v-model="loginForm.userPassword" @keyup.enter.native="onLogin" ref="pwd"></el-input>
-          <span class="svg-container svg-container_password">
-            <icon-svg icon-class="password" />
-          </span>
         </el-form-item>
-        <el-form-item :label="$t('login.remember')" label-width="80px">
+        <el-form-item label="记住密码" label-width="80px">
           <el-switch v-model="remember" @change="rememberMe"></el-switch>
         </el-form-item>
-        <el-button type="primary" @click="onLogin('loginForm')" :loading="loading">{{$t('login.login')}}</el-button>
+        <el-button type="primary" @click="onLogin()" :loading="loading">登录</el-button>
       </el-form>
     </el-card>
-    <!-- particles.js container -->
-    <div id="particles"></div>
   </el-container>
 </template>
 <script>
@@ -111,129 +107,6 @@
           }
         })
       }
-    },
-    mounted() {
-      particlesJS('particles', {
-        "particles": {
-          "number": {
-            "value": 15
-          },
-          "color": {
-            "value": "random"
-          },
-          "shape": {
-            "type": ["star", "image"],
-            "stroke": {
-              "width": 0,
-              "color": "yellow"
-            },
-            "polygon": {
-              "nb_sides": 5
-            },
-            "image": {
-              "src": "https://neveryu.github.io/avatar/avatar.png",
-              "width": 100,
-              "height": 100
-            }
-          },
-          "opacity": {
-            "value": 1,
-            "random": false,
-            "anim": {
-              "enable": true,
-              "speed": 1,
-              "opacity_min": 0.1,
-              "sync": false
-            }
-          },
-          "size": {
-            "value": 10,
-            "random": true,
-            "anim": {
-              "enable": true,
-              "speed": 10,
-              "size_min": 0.1,
-              "sync": false
-            }
-          },
-          "line_linked": {
-            "enable": false,
-            "distance": 150,
-            "color": "#ccc",
-            "opacity": 0.4,
-            "width": 1
-          },
-          "move": {
-            "enable": true,
-            "speed": 2,
-            "direction": "random",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "attract": {
-              "enable": false,
-              "rotateX": 600,
-              "rotateY": 1200
-            }
-          }
-        },
-        "interactivity": {
-          // "detect_on": "canvas",
-          "detect_on": "window",
-          "events": {
-            "onhover": {
-              "enable": false,
-              // "mode": "repulse"
-              "mode": "grab"
-            },
-            "onclick": {
-              "enable": false,
-              "mode": "repulse"
-              // "mode": "push"
-            },
-            "resize": true
-          },
-          "modes": {
-            "grab": {
-              "distance": 400,
-              "line_linked": {
-                "opacity": 1
-              }
-            },
-            "bubble": {
-              "distance": 400,
-              "size": 40,
-              "duration": 2,
-              "opacity": 8,
-              "speed": 3
-            },
-            "repulse": {
-              "distance": 200
-            },
-            "push": {
-              "particles_nb": 4
-            },
-            "remove": {
-              "particles_nb": 2
-            }
-          }
-        }
-      })
-      this.$notify({
-        title: '账号：yangweixian',
-        dangerouslyUseHTMLString: true,
-        message: '<strong>密码：<i>123456</i></strong>',
-        type: 'success',
-        position: 'bottom-left'
-      })
-      this.$notify({
-        title: '账号：yingzuidou',
-        dangerouslyUseHTMLString: true,
-        message: '<strong>密码：<i>123456</i></strong>',
-        type: 'success',
-        position: 'bottom-left',
-        offset: 80
-      })
     }
   }
 </script>
@@ -247,7 +120,19 @@
     left: 0;
     right: 0;
     bottom: 0;
-    background: mix(#494166, #424b50);
+    background: #f3f3f3;
+    .site-title {
+      position: absolute;
+      top: 50px;
+      left: calc(50% - 150px);
+      display: flex;
+      height: 60px;
+      line-height: 80px;
+      .title {
+        font-size: 18px;
+        font-weight: 700;
+      }
+    }
     .el-card {
       position: absolute;
       top: 50%;
@@ -255,13 +140,29 @@
       margin: -220px 0 0 -200px;
       width: 400px;
       height: 350px;
-      background: #d7ecf1;
+      background: #f9f9f9;
       .login-title {
         margin: 0;
         text-align: center;
       }
       .el-input /deep/ .el-input__inner {
+        padding: 0px;
         text-indent: 12px;
+      }
+      /deep/ .el-form-item__label {
+        color: black;
+      }
+      /deep/ .el-button--primary {
+        color: #FFF;
+        background-color: #409EFF;
+        border-color: #409EFF;
+      }
+      /deep/ .el-switch__core {
+        border: 1px solid #DCDFE6;
+        background: #DCDFE6;
+        &:after {
+          background-color: #FFF;
+        }
       }
       .svg-container {
         position: absolute;
@@ -280,17 +181,5 @@
         width: 100%;
       }
     }
-  }
-  #particles {
-    width: 100%;
-    height: 100%;
-    /*background-color: #b61924;*/
-    /*background-color: #23ae88;*/
-    background-color: #39c;
-    background-image: url('');
-    /*background-image: url('../img/bg_particles.png');*/
-    background-size: cover;
-    background-position: 50% 50%;
-    background-repeat: no-repeat;
   }
 </style>
