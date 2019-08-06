20c098904c97f8399f22c41bd613b0c4088b4bd9
鹰嘴豆 <1036771966@qq.com>
2019-08-06 20:57:41 +0800

    [代码优化] 代码优化

--- config/index.js
+++ config/index.js
@@ -30,8 +30,6 @@ module.exports = {
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
-
-
     /**
      * Source Maps
      */
